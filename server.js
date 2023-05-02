2ce205429275afe72a88c4e7eb04c1a2b9599d38
root <root@ip-172-31-1-100.us-east-2.compute.internal>
2023-05-02 13:59:36 +0000

    init aws

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8088;
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}.`);
 });
